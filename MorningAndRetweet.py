525f0924310dc238797512295ce9afccd877df0c
Aditya Lila <61589143+bloodtusk25@users.noreply.github.com>
2020-10-06 20:45:45 +0700

    fixed things pls

--- MorningAndRetweet.py
+++ MorningAndRetweet.py
@@ -12,7 +12,6 @@ from idolsConfig import randomIdols, idols, hashtags
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
 
-
 def main():    
 
     def goodMorning():
@@ -24,10 +23,10 @@ def main():
 
     def obliterate():
         api = create_api() 
-        counter = 0
         successfulProcess = 0
 
         for idols in hashtags:
+            counter = 0
             for i in api.search(hashtags[counter], count=2, result_type='mixed', lang='en'):
 
                 if not i.favorited:
@@ -35,7 +34,7 @@ def main():
                         i.favorite()
                         log.info(f"Liked tweet from {i.user.name}")
                     except Exception:
-                        log.error("Cant like", exc_info=True)
+                        log.error("Already Liked")
 
                 if not i.retweeted:
                     try:
@@ -43,33 +42,33 @@ def main():
                         log.info(f"Retweeted tweet from {i.user.name}")
                         successfulProcess += 1
                     except Exception:
-                        log.error("Cant like and retweet", exc_info=True)
+                        log.error("Already Retweeted")
 
                 counter += 1
                 time.sleep(1)
-                log.info("Searching next hashtag")
-        
+            log.info("Searching next hashtag") 
         log.info("Successfully processing " + str(successfulProcess) + " tweet(s)")
-        api.update_profile(description="Successfully obliterated " + str(successfulProcess) + " tweet(s) on the last operation")
+        api.update_profile(description="Successfully obliterated " + str(successfulProcess) + " tweet(s) at " + nextOperationString() + " timestamp (GMT+7)")
 
     schedule.every(2).hours.do(obliterate)
     schedule.every().day.at('06:00').do(goodMorning)
-    # formatTime = "%H:%M:%S"
+    formatTime = "%H:%M:%S"
 
-    # def timeUntilNextObliterate():
-    #     nextRun = schedule.next_run().time().strftime(formatTime)
-    #     timeNow = datetime.datetime.now().strftime(formatTime)
-    #     delta = datetime.datetime.strptime(nextRun, formatTime) - datetime.datetime.strptime(timeNow, formatTime)
+    def timeUntilNextOperation():
+        nextRun = schedule.next_run().time().strftime(formatTime)
+        timeNow = datetime.datetime.now().strftime(formatTime)
+        delta = datetime.datetime.strptime(nextRun, formatTime) - datetime.datetime.strptime(timeNow, formatTime)
 
-    #     deltafix = str(delta).replace('-1 day,','')
+        deltafix = str(delta).replace('-1 day,','')
 
-    #     return deltafix + " next until operation"
+        return deltafix + " next until operation"
 
-    # def nextObliterateString():
-    #     return str(schedule.next_run().time().strftime(formatTime))
+    def nextOperationString():
+        return str(schedule.next_run().time().strftime(formatTime))
 
     while 1:
         schedule.run_pending()
+        log.info(timeUntilNextOperation())
         time.sleep(1)
           
 if __name__ == "__main__":
