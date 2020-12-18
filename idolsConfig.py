e481e11607eb0c78dd3f6f433efe9877a944999a
Aditya Lila <61589143+bloodtusk25@users.noreply.github.com>
2020-12-18 23:23:52 +0700

    Update idolsConfig.py
    Update Idols

--- idolsConfig.py
+++ idolsConfig.py
@@ -1,11 +1,11 @@
 import random
 
 hashtags = [
-    '#아샤', '#이런', '#류진', '#예지', '#시명', '#채솔', '#쥬리', '#연희', '#수윤', '#조이', '#지효', '#지수', '#채령', '#유나', '#시현', '#미나', '#모모', '#윈터', '#아이린', '#장규리', '#이나경'
+    '#아샤', '#이런', '#류진', '#예지', '#시명', '#채솔', '#쥬리', '#연희', '#수윤', '#조이', '#지효', '#지수', '#채령', '#유나', '#시현', '#미나', '#모모', '#윈터', '#아이린', '#장규리', '#이나경', '#권은비', '#카리나', '#지젤', '#닝닝', '#강혜원', '#김민주', '#나은' , '#아린' , '#수지', '#수민' , '#아이사', '#윤', '#재이'
     ]
 
 idols = [
-    'Aisha', 'Yiren', 'Ryujin', 'Yeji', 'Simyeong', 'Chaesol', 'Juri', 'Yeonhee', 'Suyun', 'Joy', 'Jihyo', 'Jisoo', 'Chaeryeong', 'Yuna', 'Sihyeon', 'Mina', 'Momo', 'Winter', 'Irene', 'Jang Gyu-ri', 'Nagyung' 
+    'Aisha', 'Yiren', 'Ryujin', 'Yeji', 'Simyeong', 'Chaesol', 'Juri', 'Yeonhee', 'Suyun', 'Joy', 'Jihyo', 'Jisoo', 'Chaeryeong', 'Yuna', 'Sihyeon', 'Mina', 'Momo', 'Winter', 'Irene', 'Jang Gyu-ri', 'Nagyung', 'Kwon Eunbi', 'Karina', 'Giselle', 'Ningning', 'Hye Won', 'Minju', 'Naeun' , 'Arin' , 'Suzy', 'Sumin', 'Isa', 'Yoon', 'J'
     ]
 
 def randomIdols():
