0c0e3e65be85dff95aab5a125d0d22a9adf72839
Aditya Lila <61589143+bloodtusk25@users.noreply.github.com>
2021-04-10 06:35:25 +0700

    updating idols and cleaning things
    True

--- idolsConfig.py
+++ idolsConfig.py
@@ -1,9 +1,23 @@
-import random
-
 hashtags = [
-    '#아샤', '#이런', '#류진', '#예지', '#시명', '#채솔', '#쥬리', '#연희', '#수윤', '#조이', '#지효', '#지수', '#채령', '#유나', '#시현', '#미나', '#모모', '#윈터', '#아이린', '#장규리', '#이나경', '#권은비', '#카리나', '#지젤', '#닝닝', '#강혜원', '#김민주', '#나은' , '#아린' , '#수지', '#수민' , '#아이사', '#윤', '#재이', '#리아', '#희진', '#예리'
+    '#아샤', '#이런', '#류진', '#예지', '#시명', 
+    '#채솔', '#쥬리', '#연희', '#수윤', '#조이', 
+    '#지효', '#지수', '#채령', '#유나', '#시현', 
+    '#미나', '#모모', '#윈터', '#아이린', '#장규리', 
+    '#이나경', '#권은비', '#카리나', '#지젤', '#닝닝', 
+    '#강혜원', '#김민주', '#나은' , '#아린' , '#수지', 
+    '#수민' , '#아이사', '#윤', '#재이', '#리아', 
+    '#희진', '#예리', '#김제니', '#박채영', '#리사',
+    '#이브', '#비비'
     ]
 
 idols = [
-    'Aisha', 'Yiren', 'Ryujin', 'Yeji', 'Simyeong', 'Chaesol', 'Juri', 'Yeonhee', 'Suyun', 'Joy', 'Jihyo', 'Jisoo', 'Chaeryeong', 'Yuna', 'Sihyeon', 'Mina', 'Momo', 'Winter', 'Irene', 'Gyuri', 'Nagyung', 'Eunbi', 'Karina', 'Giselle', 'Ningning', 'Hyewon', 'Minju', 'Naeun' , 'Arin' , 'Suzy', 'Sumin', 'Isa', 'Yoon', 'J', 'Lia', 'Heejin', 'Yeri' 
+    'Aisha', 'Yiren', 'Ryujin', 'Yeji', 'Simyeong', 
+    'Chaesol', 'Juri', 'Yeonhee', 'Suyun', 'Joy', 
+    'Jihyo', 'Jisoo', 'Chaeryeong', 'Yuna', 'Sihyeon', 
+    'Mina', 'Momo', 'Winter', 'Irene', 'Gyuri', 
+    'Nagyung', 'Eunbi', 'Karina', 'Giselle', 'Ningning', 
+    'Hyewon', 'Minju', 'Naeun' , 'Arin' , 'Suzy', 
+    'Sumin', 'Isa', 'Yoon', 'J', 'Lia', 
+    'Heejin', 'Yeri', 'Jennie', 'Rosé', 'Lisa',
+    'Yves', 'Vivi'
     ]
